9aed5d459aa706121fbea4d84f58663736ab2dc5
Dart <skulldude327@gmail.com>
2025-07-24 16:31:38 -0500

    Remove cigs compat, added in cigs

--- addons/compat_cigs/README.md
+++
@@ -1,4 +0,0 @@
-hoa_compat_cigs
-===================
-
-Prevents Immsersion Cigs's nvg versions of cigarretes from being slung with your helmet.
--- addons/compat_cigs/script_component.hpp
+++
@@ -1,9 +0,0 @@
-#define COMPONENT compat_cigs
-#define COMPONENT_BEAUTIFIED Immersion Cigs Compatibility
-#include "\z\hoa\addons\main\script_mod.hpp"
-
-// #define DEBUG_MODE_FULL
-// #define DISABLE_COMPILE_CACHE
-// #define ENABLE_PERFORMANCE_COUNTERS
-
-#include "\z\hoa\addons\main\script_macros.hpp"
